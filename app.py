c21dd46158a28df4754422ab40991e936d6c4d2a
Nathan Ashta <nathanashta@gmail.com>
2019-07-28 18:00:08 -0500

    fixing weather

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def webhook():
                 "https://api.openweathermap.org/data/2.5/weather?q=Chicago&APPID=3887bd167e909f96b57808bda8f98bbd") \
                 as response:
             weather = response.read();
-            print(weather)
+            print(weather["main"])
         send_message(weather["main"]["temp"])
     return "ok", 200
 
